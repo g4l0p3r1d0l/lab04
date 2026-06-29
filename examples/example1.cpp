c19b52f3123e4e4b5fea39950ace49f3e6e967a8
g4l0p3r1d0l <utockakra916@gmail.com>
2026-06-29 22:20:27 +0300

    feat: setup github actions workflow

---
+++ examples/example1.cpp
@@ -0,0 +1,7 @@
+#include <print.hpp>
+#include <iostream>
+int main() {
+    print("Hello from Lab 03!", std::cout);
+    std::cout << std::endl;
+    return 0;
+}
---
+++ include/print.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <string>
+#include <ostream>
+void print(const std::string& text, std::ostream& out);
